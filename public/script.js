c6b79f0d6c1c43dd423dee283e3ca76183d1c732
gfrey-utn <reygastonf@yahoo.com.ar>
2024-11-07 20:49:46 -0300

    Arreglado error al eliminar

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const boton = '<button class="w-1/6 min-w-fit bg-red-600 text-white flex justify-center items-center rounded"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6"> <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" /> </svg> </button>';
+const iconoTacho = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6"> <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" /> </svg>' 
 
 const getAlbums =  async () => {
   try {
@@ -61,13 +61,45 @@ const renderAlbums = (album) => {
   p.classList.add('font-semibold', 'italic', 'w-5/6', 'bg-violet-800', 'text-center')
   p.textContent = album["año"]
   renglon.appendChild(p)
-  renglon.insertAdjacentHTML("beforeend", boton)
+  // renglon.insertAdjacentHTML("beforeend", boton)
 
+  const deleteButton = document.createElement('button')
+  deleteButton.classList.add('w-1/6', 'min-w-fit', 'bg-red-600', 'text-white', 'flex', 'justify-center', 'items-center', 'rounded')
+  deleteButton.insertAdjacentHTML("beforeend", iconoTacho)
+  renglon.appendChild(deleteButton)
+
+  deleteButton.addEventListener('click', () => deleteAlbum(album._id))
+
+}
+
+const deleteAlbum = async (id) => {
+  try {
+    const albumABorrar = await axios.get('https://proyectodisco.onrender.com/band/' + id)
+    const nombreDelAlbum = albumABorrar.nombre
+    await axios.delete('https://proyectodisco.onrender.com/band/' + id)
+    swal({
+        title: '¡Álbum borrado!',
+        text: 'Borraste ' + nombreDelAlbum + 'de la lista.',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+    })
+  } catch(error) {
+    console.log(error)
+    swal({
+        title: '¡Error!',
+        text: 'No se ha podido eliminar el álbum.',
+        icon: 'error',
+        button: 'Ok'
+    })
+  }
+  redirect('https://proyectodisco.onrender.com/')
 }
 
 getAlbums();
 
-const redirect = (id) => { window.location.href = `./album.html?album=${id}`}
+function redirect(url) {
+  window.location.href = url;
+}
 
 let favoriteRecords = ["A Night at the Opera", "Greatest Hits", "Greatest Hits II"];
 
